added pytorch imports, fixed gradient calculation

src/gradient_penalty.py
@@ -1,3 +1,7 @@
+import torch
+from torch.autograd import Variable
+import torch.autograd
+
 def gradient_penalty(Y_label, Y_predicted, discriminator, gp_weight):
         """
         Y_label: The expected results of the NN
@@ -15,7 +19,7 @@ def gradient_penalty(Y_label, Y_predicted, discriminator, gp_weight):
 
         prob_interpolated = discriminator(interpolated)
         
-        gradients = torch_grad(outputs=prob_interpolated, inputs=interpolated,
+        gradients = torch.autograd.grad(outputs=prob_interpolated, inputs=interpolated,
                                grad_outputs=torch.ones(prob_interpolated.size()),
                                create_graph=True, retain_graph=True)[0]
 