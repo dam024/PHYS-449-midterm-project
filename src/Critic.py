Fixed gradient problem

src/Critic.py
@@ -63,8 +63,8 @@ class Critic(nn.Module):
 
     def prepareForBackprop(self, generator):
         # Allow no weight change of generator
-        #for p in generator.parameters():
-            #p.requires_grad = False
+        for p in generator.parameters():
+            p.requires_grad = False
         # allows weight update of critic
         for p in self.parameters():
             p.requires_grad = True

src/Generator.py
@@ -99,7 +99,7 @@ class Generator(nn.Module):
         for p in critic.parameters():
             p.requires_grad = False
             #allows weight update of generator
-        #for p in self.parameters():
-            #p.requires_grad = True
+        for p in self.parameters():
+            p.requires_grad = True
 
         